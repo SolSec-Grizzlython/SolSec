46dfc34f746abc37b2994d0cd8268f1ee4734781
satyaraj rana <ranasatyaraj7103iitr@gmail.com>
2023-03-12 10:44:42 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <LandingPage />
-    </div>
-  );
+      
+        <Routes>
+          <Route path="/" Component={LandingPage} />
+        </Routes>
+  
+    );
 }
 
 export default App;
