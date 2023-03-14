06b48a97947950f4ff963d8cdfdc9d20ec4cdc24
Aman Jat <amanjat@Amans-MacBook-Air-3.local>
2023-03-14 17:48:28 +0530

    Compete Page

--- src/Components/Competitions/Competitions.jsx
+++ src/Components/Competitions/Competitions.jsx
@@ -2,45 +2,9 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import "./Competitions.css";
 import CompetitionCard from "./CompetitionCards/CompetitionCard";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function Competitions() {
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+ 
   return (
     <>
       <Navbar />
@@ -55,9 +19,7 @@ export default function Competitions() {
                 </div>
               </div>
               <div className="competitions-card">
-                <Slider {...settings}>
                   <CompetitionCard />
-                </Slider>
               </div>
             </div>
             <div className="competitions-info">
